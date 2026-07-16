da92c0a00453dd9394fc3a60b98c269578444423
Dho99 <ridhoawwaludin@gmail.com>
2026-07-16 15:19:29 +0700

    feat: login page + remake theme and dashboard layout referred to design

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import AdminDashboard from "./AdminDashboard";
-import AdminSidebar from "./components/pages/AdminSidebar";
 import { SidebarProvider } from "./components/ui/sidebar";
-import AdminNavbar from "./components/pages/AdminNavbar";
+import AdminMiddleware from "./AdminMiddleware";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./contexts/AuthContext";
+import { Routes, Route } from "react-router";
 
 export default function App() {
     return (
-        <SidebarProvider defaultOpen={true}>
-            <AdminSidebar />
-            <div className="w-full">
-                <AdminNavbar />
-                <div className="container mx-auto py-8 px-5">
-                    <AdminDashboard />
-                </div>
-            </div>
-        </SidebarProvider>
+        <AuthProvider>
+            <SidebarProvider defaultOpen={true}>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route
+                        path="/*"
+                        element={
+                            <AdminMiddleware>
+                                <AdminDashboard />
+                            </AdminMiddleware>
+                        }
+                    />
+                </Routes>
+            </SidebarProvider>
+        </AuthProvider>
     );
 }
